e0bce0dc9af41c0e6d8ab3c79ab56e5c760a58b4
Shamil Ganiev <ganiev@pm.me>
2023-12-08 20:55:51 +0400

    up

--- examples/provider-install-verification/main.tf
+++ examples/provider-install-verification/main.tf
@@ -20,6 +20,13 @@ resource "temporal_namespace" "hurma" {
   name        = "hurma"
   description = "This is a test description"
 }
+resource "temporal_namespace" "test" {
+  name        = "asdf"
+  description = "This is a test description"
+}
+output "one_new_namespace" {
+  value = temporal_namespace.test
+}
 output "namespace" {
   value = data.temporal_namespace.test
 }
--- internal/provider/namespace_resource.go
+++ internal/provider/namespace_resource.go
@@ -9,6 +9,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/listdefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"go.temporal.io/api/enums/v1"
@@ -70,6 +72,9 @@ func (r *NamespaceResource) Schema(ctx context.Context, req resource.SchemaReque
 			"id": schema.StringAttribute{
 				MarkdownDescription: "Namespace identifier",
 				Computed:            true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
 			},
 			"description": schema.StringAttribute{
 				MarkdownDescription: "Namespace Description",

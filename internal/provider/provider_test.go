1f81123469cd5c9d47864f7d07d02f55e5355f87
Shamil Ganiev <ganiev@pm.me>
2023-12-12 19:28:52 +0400

    test package

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -1,6 +1,8 @@
-package provider
+package provider_test
 
 import (
+	"terraform-provider-temporal/internal/provider"
+
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 )
@@ -19,5 +21,5 @@ provider "temporal" {
 // CLI command executed to create a provider server to which the CLI can
 // reattach.
 var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
-	"temporal": providerserver.NewProtocol6WithError(New("test")()),
+	"temporal": providerserver.NewProtocol6WithError(provider.New("test")()),
 }

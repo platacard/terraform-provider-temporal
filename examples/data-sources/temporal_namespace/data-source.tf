ec7f2cb123d29338727afd8adc6715650bec77e1
Shamil Ganiev <ganiev@pm.me>
2023-12-12 13:42:42 +0400

    up

--- examples/data-sources/temporal_namespace/data-source.tf
+++ examples/data-sources/temporal_namespace/data-source.tf
@@ -1,3 +1,4 @@
+# Get data of the example namespace
 data "temporal_namespace" "example" {
   name = "example"
 }
--- examples/resources/temporal_namespace/resource.tf
+++ examples/resources/temporal_namespace/resource.tf
@@ -1,3 +1,4 @@
+# Manage an example namespace.
 resource "temporal_namespace" "example" {
   name        = "example"
   description = "This is example namespace"

8eb9743f11509d4581b51f5db382a581178aad56
Ami Baid <amibaid14@gmail.com>
2024-07-15 11:40:04 -0700

    Updated search attribute data source tests

--- internal/provider/search_attribute_data_source_test.go
+++ internal/provider/search_attribute_data_source_test.go
@@ -11,24 +11,20 @@ func TestAccSearchAttributeDataSource(t *testing.T) {
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 
 		Steps: []resource.TestStep{
+			// Test reading from the server and setting data source attributes
 			{
-				Config: testAccSearchAttributeDataSourceConfig("default", "RunId"),
+				Config: providerConfig + `
+                data "temporal_search_attribute" "example" {
+                    name = "RunId"
+                    namespace = "default"
+                }`,
+
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("data.temporal_search_attribute.example", "name", "RunId"),
-					resource.TestCheckResourceAttr("data.temporal_search_attribute.example", "type", "Keyword"),
+					resource.TestCheckResourceAttr("data.temporal_search_attribute.example", "type", "Keyword"), // Verify that the type was read correctly from the server
 					resource.TestCheckResourceAttr("data.temporal_search_attribute.example", "namespace", "default"),
 				),
 			},
 		},
 	})
-}
-
-// Helper function to create Terraform configuration string
-func testAccSearchAttributeDataSourceConfig(namespace, attributeName string) string {
-	return providerConfig + `
-data "temporal_search_attribute" "example" {
-	name = "` + attributeName + `"
-	namespace = "` + namespace + `"
-}
-`
 }
